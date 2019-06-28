aee7d9103b372ed0224741dc5224e23421fa73d4
ali alghubari <alghubari.772017@gmail.com>
2019-06-28 18:36:41 +0300

    second script

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
-     
-  var arr = []; // List of users 
- var uar =[];
+	
  
+  var arr = []; // List of users 
+
+
  setInterval(function(){
  update_last_activity(); 
-fetch_friend();
 
+ load_unseen_notification();
  
   },5000);
- 
+  
  $(document).on('click', '.msg_head', function() { 
   var chatbox = $(this).parents().attr("rel") ;
   $('[rel="'+chatbox+'"] .msg_wrap').slideToggle('slow');
   return false;
  });
- 
  
  $(document).on('click', '.close', function() { 
   var chatbox = $(this).parents().parents().attr("rel") ;
@@ -30,6 +30,8 @@ fetch_friend();
   var userID = $(this).attr("class");
   var uID = $(this).attr("ruid");
   var username = $(this).children().text() ;
+  var userimg = $(this).attr("imgid");
+
   
   if ($.inArray(userID, arr) != -1)
   {
@@ -37,42 +39,24 @@ fetch_friend();
      }
   
   arr.unshift(userID);
-  chatPopup =  '<div class="msg_box" style="right:270px" ruid="'+ uID +'" rel="'+ userID +'">'+
+  chatPopup =  '<div class="msg_box" style="right:270px" ruid="'+uID+'" rel="'+userID+'">'+
      '<div class="msg_head">'+username +
-     '<div class="close">x</div> </div>'+
-     '<div class="msg_wrap"> <div class="msg_body"> <div class="msg_push'+ userID +'"></div> </div>'+
-     '<div class="msg_footer"><textarea class="msg_input" rows="4"></textarea></div>  </div>  </div>' ;     
+     '<div class="close"> x  </div></div>'+
+     '<div class="msg_wrap"> <div class="msg_body"> <div class="msg_push'+userID+'"></div> </div>'+
+     '<div class="msg_footer"><textarea  class="msg_input" rows="4"></textarea></div>  </div>  </div>' ;     
     
      $("body").append(  chatPopup  );
+
   displayChatBox();
-  display_message(uID,userID);
+  
+ 
+ 
+ 
+ 
  });
  
  
- function load_data(msg,uiD,userID)
-	{
-		$.ajax({
-			url:"insertChat.php",
-			method:"post",
-			data:{fuid:uiD,touid:userID,messge:msg},
-			
-		});
-	}
-	
-	function display_message(uiD,userID)
-
-	{
-		
-		$.ajax({
-			url:"displaychat.php",
-			method:"post",
-			data:{fuid:uiD,touid:userID},
-			success:function(data)
-			{
-				$('.msg_push'+userID).html(data);
-			}
-		});
-	}
+ 
  
  function update_last_activity(){
 	 
@@ -101,12 +85,101 @@ $.ajax({
 			
 		});	 
  }
- $(document).on('keypress', 'textarea' , function(e) { 
+ 
+ 
+ 
+ function load_data(msg,uiD,user)
+	{
+		$.ajax({
+			url:"insertChat.php",
+			method:"post",
+			data:{fuid:uiD,touid:user,messge:msg},
+			success:function(data)
+			{
+			 
+   	
+				
+			}
+			
+		});
+	}
+	function update_ReadMessage(user)
+	{
+		$.ajax({
+			url:"readMessage.php",
+			method:"post",
+			data:{touid:user},
+			success:function(data)
+			{
+			$('.msg_push'+user).append(data);  
+   	
+			}
+			
+		});
+	}
+	
+	function display_message(userID)
+
+	{
+		
+		$.ajax({
+			url:"displaychat.php",
+			method:"post",
+			data:{touid:userID},
+			success:function(data)
+			{
+			  
+ $('.msg_push'+userID).html(''); 
+ //$('.msg-right').html('');
+// $('.msg-left').html('').hide(); 
+  $('.msg_push'+userID).append(data); 
+
+   var objDiv = $('.msg_body');
+    if (objDiv.length > 0){
+       
+		 var height = objDiv[0].scrollHeight;
+ objDiv.scrollTop(height);
+    }  
+   
+				
+			}
+		});
+	}
+	
+
+	function update_display_message(userID)
+
+	{
+	 
+  $('msg_push'+userID).each(function(){
+	 var to_user_id = $(this).data('touid');
+    display_message(to_user_id);
+	
+  });    
+   
+				
+			
+		
+	}
+	 
+		 
+ $(document).on('click','textarea',function(){
+		 var user = $(this).parents().parents().parents().attr("rel"); 
+        if ($(this).is(":focus")) {
+         update_ReadMessage(user);
+		
+        }
+ });
+ 
+ $(document).on('keypress', 'textarea' , function(e) {
+ 	 
 
         if (e.keyCode == 13 ) {   
+	
             var msg = $(this).val();  
    $(this).val('');
-    var userID = $(".msg_box").attr("rel");
+  // var user =arr[0];
+ 
 	 var uiD = $(".msg_box").attr("ruid");
 	
  //  var username = $(this).parents().parents().parents().attr("class").nextSibling();
@@ -114,15 +187,25 @@ $.ajax({
   // var username  = oldStr.substr(0, oldStr.length-2);
   var username = uiD;
    if(msg.trim().length != 0){ 
+   var user = $(this).parents().parents().parents().attr("rel");
    	
-   	
-     load_data(msg,uiD,userID);
+     load_data(msg,uiD,user);
 	
    var chatbox = $(this).parents().parents().parents().attr("rel") ;
-  $('<div class="msg-right">'+msg+'</div>').insertBefore('[rel="'+chatbox+'"] .msg_push');
-   $('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
-   // display_message(uID,userID);
+
+  //$('<div class="msg-right"></div>').insertAfter('[rel="'+chatbox+'"] .msg_push'+user+'');
+  
+   var objDiv = $('.msg_body');
+    if (objDiv.length > 0){
+       
+		 var height = objDiv[0].scrollHeight;
+ objDiv.scrollTop(height);
+    }
+
+   //display_message(uID,user);
+    // alert(user);
    }
+  
    
         }
     });
@@ -130,19 +213,55 @@ $.ajax({
   
     
  function displayChatBox(){ 
+
      i = 270 ; // start position
   j = 260;  //next position
   
   $.each( arr, function( index, value ) {  
      if(index < 4){
-          $('[rel="'+value+'"]').css("right",i);
+		
+     
+    $('[rel="'+value+'"]').css("right",i);
     $('[rel="'+value+'"]').show();
-       i = i+j;    
+       i = i+j; 
+       
      }
      else{
     $('[rel="'+value+'"]').hide();
      }
         });  
- }  
+ }
+
+
+ function load_unseen_notification(view = '')
+ {
+  $.ajax({
+   url:"unReadMessage.php",
+   method:"POST",
+   data:{view:view},
+   dataType:"json",
+   success:function(data)
+   {
+    $('.dropdown-menu').html(data.notification);
+    if(data.unseen_notification > 0)
+    {
+     $('.count').html(data.unseen_notification);
+    }
+   }
+  });
+ }
  
+  $(document).on('click', '.dropdown-toggle', function(){
+  $('.count').html('');
+  load_unseen_notification('yes');
+ });
+ 
+
+
+ 
+ 
+
+
+ 
+
 });
